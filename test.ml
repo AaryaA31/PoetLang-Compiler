011ff2cc9a3d3c8ab88d5622e379109e00ffe4c6
dchoi127 <dchoi127@terpmail.umd.edu>
2025-04-07 21:55:11 -0400

    using correct start symbol

--- test.ml
+++ test.ml
@@ -1,8 +1,8 @@
-
 open Ast
 
 let () =
   let lexbuf = Lexing.from_channel stdin in
-  let stmts = Parser.program Scanner.token lexbuf in
-  let output = String.concat "" (List.map string_of_stmt stmts) in
+  (* Assuming Parser.program returns a (bind list * func_def list) tuple *)
+  let prog = Parser.program Scanner.token lexbuf in
+  let output = string_of_program prog in
   print_endline output
